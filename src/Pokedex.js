8c2206d887405ef59d7e1956c6d19e507d462e3b
marsleite <marsleite@gmail.com>
2021-05-31 07:09:21 -0300

    parte 1 - pokedex done

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,9 +4,26 @@ import Pokemon from './Pokemon';
 
 
 class Pokedex extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      index: 0
+    }
+    this.nextPoke = this.nextPoke.bind(this)
+  }
+
+  nextPoke() {
+    this.setState((previous) => ({
+      index: (previous.index === pokemons.length - 1) ? 0 : previous.index + 1
+    }))
+  }
+
   render() {
     return (
-      pokemons.map((poke, index) => <Pokemon info={ poke } key={ index } />)
+      <div>
+        <Pokemon info={ pokemons[this.state.index] } />
+        <button onClick={this.nextPoke}>next</button>
+      </div>
     );
   }
 };
